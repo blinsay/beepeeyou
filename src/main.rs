c1fa559e51c938e4d9403a8c668ca139ddc73bef
Ben Linsay <blinsay@gmail.com>
2018-05-07 20:16:37 -0400

    clean up this monstrosity

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
-#![allow(dead_code, unused_variables, unused_imports, unused_mut)]
 extern crate cpal;
 extern crate libc;
 
-use std::f32::consts::PI;
 use libc::c_int;
+use std::f32::consts::PI;
 use std::io;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::thread::{sleep, spawn};
 use std::time::Duration;
+
+const LOAD_MULT: f64 = 10.0;
+
+fn main() {
+    let load = Arc::new(AtomicUsize::new(LOAD_MULT as usize));
+
+    // start a thread that gets one minute loadavg from the system and stores it
+    // as an atomic uint. to convert from the float to an int, multiply by
+    // LOAD_MULT and round.
+    let write_load = load.clone();
+    let set_load = move || loop {
+        if let Ok((one_min, _five, _fifteen)) = load_avg() {
+            let rounded = (one_min * LOAD_MULT as f64).round() as usize;
+            write_load.store(rounded, Ordering::Release);
+        }
+        sleep(Duration::from_secs(1));
+    };
+
+    // read load averages from the same atomic uint by pulling them out and
+    // dividing by the LOAD_MULT constant.
+    let read_load = load.clone();
+    let get_load = move || (read_load.load(Ordering::Acquire) as f64) / LOAD_MULT;
+
+    // in a background thread, get load and set it every second
+    spawn(set_load);
+
+    // start the audio event loop and write beeps to it
+    let (event_loop, stream_id, format) =
+        default_device_loop().expect("couldn't set up the default audio loop!");
+    output_beepeeyou(event_loop, stream_id, &format, || get_load() as f32);
+}
 
 #[derive(Debug)]
 enum BeepError {
@@ -16,27 +46,7 @@ enum BeepError {
     DeviceError { reason: &'static str },
 }
 
-fn main() {
-    let load_mult: f32 = 10.0;
-    let load = Arc::new(AtomicUsize::new(1));
-    let current_frequency = Arc::new(AtomicUsize::new(440));
-
-    let write_load = load.clone();
-    spawn(move || loop {
-        set_load(load_mult as f64, &write_load);
-        sleep(Duration::from_secs(1));
-    });
-
-    let (event_loop, stream_id, format) =
-        default_device_loop().expect("couldn't set up the default audio loop!");
-
-    let read_load = load.clone();
-    let sample_rate = format.sample_rate.0 as f32;
-    output_beepeeyou(event_loop, stream_id, &format, move || {
-        get_load(load_mult as f64, &read_load) as f32
-    });
-}
-
+// get an event loop configured to output to the default audio device
 fn default_device_loop() -> Result<(cpal::EventLoop, cpal::StreamId, cpal::Format), BeepError> {
     use cpal::{CreationError, DefaultFormatError};
     use BeepError::*;
@@ -66,6 +76,7 @@ fn default_device_loop() -> Result<(cpal::EventLoop, cpal::StreamId, cpal::Forma
     Ok((event_loop, stream_id, output_format))
 }
 
+// beep beep, i'm a jeep
 fn output_beepeeyou<T>(
     event_loop: cpal::EventLoop,
     output_stream: cpal::StreamId,
@@ -81,6 +92,17 @@ where
     let mut sample_clock = 0f32;
     let mut frequency = 220f32 * get_frequency();
 
+    let mut calculate_next_sample = move || {
+        let next_sample = ((sample_clock / sample_rate) * frequency * 2.0 * PI).sin();
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+
+        if sample_clock % 1000.0 == 0.0 {
+            frequency = 220f32 * get_frequency();
+        }
+
+        next_sample
+    };
+
     event_loop.run(move |id, data| {
         if id != output_stream {
             return;
@@ -90,72 +112,35 @@ where
         use cpal::UnknownTypeOutputBuffer::*;
 
         match data {
-            Output { buffer: U16(mut buf) } => {
-                for sample in buf.chunks_mut(channels) {
-                    let next_sample = ((sample_clock / sample_rate) * frequency * 2.0 * PI).sin();
-                    sample_clock = (sample_clock + 1.0) % sample_rate;
-
-                    if sample_clock % 1000.0 == 0.0 {
-                        frequency = 220f32 * get_frequency();
-                    }
-
-                    for out in sample.iter_mut() {
-                        *out = ((next_sample * 0.5 + 0.5) * (std::u16::MAX as f32)) as u16;
-                    }
-                }
-            },
-            Output { buffer: F32(mut buf) } => {
-                for sample in buf.chunks_mut(channels) {
-                    let next_sample = ((sample_clock / sample_rate) * frequency * 2.0 * PI).sin();
-                    sample_clock = (sample_clock + 1.0) % sample_rate;
-
-                    if sample_clock % 1000.0 == 0.0 {
-                        frequency = 220f32 * get_frequency();
-                    }
-
-                    for out in sample.iter_mut() {
-                        *out = next_sample;
-                    }
-                }
-            },
+            Output { buffer: U16(buf) } => {
+                write_samples(buf, channels, &mut calculate_next_sample);
+            }
+            Output { buffer: I16(buf) } => {
+                write_samples(buf, channels, &mut calculate_next_sample);
+            }
+            Output { buffer: F32(buf) } => {
+                write_samples(buf, channels, &mut calculate_next_sample);
+            }
             _ => (),
         }
     })
 }
 
-// fn write_samples<S, T>(
-//     buf: &mut cpal::OutputBuffer<S>,
-//     clock: f32,
-//     sample_rate: f32,
-//     channels: usize,
-//     frequency: f32
-// ) -> f32
-// where
-//     S: cpal::Sample,
-//     T: FnMut(f32) -> f32,
-// {
-//     let mut clock = clock;
-//     for sample in buf.chunks_mut(channels) {
-//         let value = next_value(clock);
-//         for out in sample.iter_mut() {
-//             *out = S::from(&value);
-//         }
-//         clock = (clock + 1.0) % sample_rate;
-//     }
-//     clock
-// }
-
-fn set_load(mult: f64, into: &AtomicUsize) {
-    if let Ok((one_min, _five, _fifteen)) = load_avg() {
-        let rounded = (one_min * mult).round() as usize;
-        into.store(rounded, Ordering::Release);
+// call get_next_sample to fill the sample buffer
+fn write_samples<S, F>(mut buf: cpal::OutputBuffer<S>, channels: usize, get_next_sample: &mut F)
+where
+    S: cpal::Sample,
+    F: FnMut() -> f32,
+{
+    for sample in buf.chunks_mut(channels) {
+        let next_sample = get_next_sample();
+        for out in sample.iter_mut() {
+            *out = cpal::Sample::from(&next_sample);
+        }
     }
 }
 
-fn get_load(mult: f64, from: &AtomicUsize) -> f64 {
-    (from.load(Ordering::Acquire) as f64) / mult
-}
-
+// use libc to get loadavg
 fn load_avg() -> io::Result<(f64, f64, f64)> {
     let mut loads: [f64; 3] = [0.0; 3];
     let loaded = unsafe { getloadavg(&mut loads[0], 3) };
